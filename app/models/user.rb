f6fba4a9113af676a6211ec521bb3f0fa8209c97
Dylan Cianci <cianci.dy@gmail.com>
2023-09-04 18:06:13 +0200

    ajout associations table users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Associations candidats
+  has_many :applications, dependent: :destroy
+  has_many :job_offers_as_candidate, through: :applications, source: :job_offers
+  # Associations recruteurs
+  has_many :job_offers_as_recruiter, through: :companies, source: :job_offers
+  has_many :companies, optional: true
+  has_many :feedbacks, dependent: :destroy
 end
